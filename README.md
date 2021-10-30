97c7ab32b92fa8ffd70b95b7a24449571e974750
vincentmin <vincentmin17@gmail.com>
2021-10-30 23:46:36 +0200

    added bertopic analysis of paper abstracts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PaperClustering
 Use NLP to automatically extract information such as common topics from sets of papers.
+
+For use on windows follow the following steps:
+- update to windows 11
+- setup WSL2 as described here: https://docs.microsoft.com/en-us/windows/wsl/setup/environment
+- For gpu support download and install the driver from https://developer.nvidia.com/cuda/wsl
--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 import os
+
 requirements_path = os.path.join(
-    os.path.dirname(
-        os.path.realpath(__file__)
-    ),
-    "requirements.txt"
+    os.path.dirname(os.path.realpath(__file__)), "requirements.txt"
 )
 
 with open(requirements_path) as requirements_file:
@@ -15,6 +13,6 @@ setup(
     version="0.1",
     description="Package for analyzing and extracting insight from a large collection of papers",
     author="Vincent Min",
-    packages = ["src"],
-    install_requires=requirements
+    packages=["src"],
+    install_requires=requirements,
 )
